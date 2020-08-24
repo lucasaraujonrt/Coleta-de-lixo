a0ba097c9dbf9e06c2f03ad5d7164981d1a78ded
lucasaraujonrt <lucasaraujo8186@gmail.com>
2020-08-24 17:45:38 -0300

    add sass and responsivily screen

--- front/src/pages/Provider/index.js
+++ front/src/pages/Provider/index.js
@@ -1,58 +1,117 @@
 import React from 'react';
-import { Input, Dropdown, Button } from 'antd';
+import { Input, Dropdown, Button, Row, Col, Divider } from 'antd';
+import './styles.scss'
 
 
 export default function Provider() {
   return (
     <div className="provider_container">
-      <div className="provider_title">
+      <div className="provider_container__title">
         <h1>Cadastro de fornecedores</h1>
       </div>
-      <form className="provider_form">
-        <div className="provider_form_input">
-          <label>Razão social</label>
-          <Input placeholder="Solteiro" />
+
+      <Divider />
+
+      <form className="provider-form">
+        <div className="provider-form__input">
+          <Row gutter={16}>
+            <Col span={8}>
+              <label>Razão social</label>
+              <Input placeholder="Solteiro" />
+            </Col>
+          </Row>
         </div>
-        <div className="provider_form_input">
-          <label>CNPJ/CPF</label>
-          <Input placeholder="508.127.628-60" />
-          <label>I.E</label>
-          <Input placeholder="508.127.628-60" />
+
+        <div className="provider-form__input">
+          <Row gutter={16}>
+            <Col span={8}>
+              <label>CNPJ/CPF</label>
+              <Input placeholder="508.127.628-60" />
+            </Col>
+            <Col span={8}>
+              <label>I.E</label>
+              <Input placeholder="508.127.628-60" />
+            </Col>
+          </Row>
         </div>
-        <div className="provider_form_input">
-          <label>Endereço</label>
-          <Input placeholder="Av Marechal Rondon" />
+
+        <div className="provider-form__input">
+          <Row gutter={16}>
+            <Col span={8}>
+              <label>Endereço</label>
+              <Input placeholder="Av Marechal Rondon" />
+            </Col>
+          </Row>
         </div>
-        <div className="provider_form_input">
-          <label>Número</label>
-          <Input placeholder="2044" />
-          <label>Complemento</label>
-          <Input placeholder="apto 33" />
+
+        <div className="provider-form__input">
+          <Row gutter={16}>
+            <Col span={8}>
+              <label>Número</label>
+              <Input placeholder="2044" />
+            </Col>
+            <Col span={8}>
+              <label>Complemento</label>
+              <Input placeholder="apto 33" />
+            </Col>
+          </Row>
         </div>
-        <div className="provider_form_input">
-          <label>Bairro</label>
-          <Input placeholder="Jardin Chapadão" />
-          <label>CEP</label>
-          <Input placeholder="13070-172" />
+
+        <div className="provider-form__input">
+          <Row gutter={16}>
+            <Col span={8}>
+              <label>Bairro</label>
+              <Input placeholder="Jardin Chapadão" />
+            </Col>
+            <Col span={8}>
+              <label>CEP</label>
+              <Input placeholder="13070-172" />
+            </Col>
+          </Row>
         </div>
-        <div className="provider_form_input">
-          <label>Munícipio</label>
-          <Input placeholder="Campinas" />
-          <Dropdown placement="bottomLeft" arrow>
-            <Button>UF</Button>
-          </Dropdown>
+
+        <div className="provider-form__input">
+          <Row gutter={16}>
+            <Col span={8}>
+              <label>Munícipio</label>
+              <Input placeholder="Campinas" />
+            </Col>
+            <Col span={8}>
+              <Dropdown placement="bottomLeft" arrow>
+                <Button>UF</Button>
+              </Dropdown>
+            </Col>
+          </Row>
         </div>
-        <div className="provider_form_input">
-          <label>Fone</label>
-          <Input placeholder="(19) 99755-2541" />
-          <label>E-mail</label>
-          <Input placeholder="lucas.araujo@mblabs.com" />
+
+        <div className="provider-form__input">
+          <Row gutter={16}>
+            <Col span={8}>
+              <label>Fone</label>
+              <Input placeholder="(19) 99755-2541" />
+            </Col>
+            <Col span={8}>
+              <label>E-mail</label>
+              <Input placeholder="lucas.araujo@mblabs.com" />
+            </Col>
+          </Row>
         </div>
-        <div className="provider_form_button">
-          <Button type="primary">Procurar</Button>
-          <Button type="primary">Incluir</Button>
-          <Button type="primary" danger>Excluir</Button>
-          <Button type="primary">Salvar</Button>
+
+        <div className="provider-form__button">
+          <Row gutter={16}>
+            <Col span={6}>
+              <Button type="primary">Procurar</Button>
+            </Col>
+            <Col span={6}>
+              <Button type="primary">Incluir</Button>
+            </Col>
+            <Col span={6}>
+              <Button type="primary" danger>Excluir</Button>
+            </Col>
+            <Col span={6}>
+              <Button type="primary">Salvar</Button>
+            </Col>
+          </Row>
         </div>
       </form>
     </div>
