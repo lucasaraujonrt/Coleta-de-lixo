dc63133633e3c19e144bb613a807f371693dde93
lucasaraujonrt <lucasaraujo8186@gmail.com>
2020-08-27 00:05:29 -0300

    IMPROVE CSS

--- front/src/pages/Provider/index.js
+++ front/src/pages/Provider/index.js
@@ -5,19 +5,20 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './styles.scss';
 import api from '../../api';
-// import AdvancedSideBar from '../../components/AdvancedSideBar'
+
 
 export default function Provider() {
-  const [social, setSocial] = useState("");
-  const [documents, setDocuments] = useState("");
-  const [ie, setIe] = useState("");
+  const [company_name, setCompany_name] = useState("");
+  const [cnpj, setCnpj] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [state_registration, setState_registration] = useState("");
   const [address, setAddress] = useState("");
   const [number, setNumber] = useState();
-  const [complements, setComplements] = useState("");
-  const [district, setDistrict] = useState("");
+  const [complement, setComplement] = useState("");
+  const [neighborhood, setNeighborhood] = useState("");
   const [cep, setCep] = useState("");
   const [city, setCity] = useState("");
-  const [uf, setUf] = useState("");
+  const [state, setState] = useState("");
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
 
@@ -26,31 +27,34 @@ export default function Provider() {
   async function handleSubmit(e) {
     e.preventDefault();
     const data = {
-      social,
-      documents,
-      ie,
+      company_name,
+      cpf,
+      cnpj,
+      state_registration,
       address,
       number,
-      complements,
-      district,
+      complement,
+      neighborhood,
       cep,
       city,
-      uf,
+      state,
       phone,
       email
     }
     try {
       await api.post('providers', data);
+      toast.success('Fornecedor cadastrado com sucesso!!')
+
     }
-    catch{
-      toast.error('Erro ao cadastrar fornecedor, tente novamenta mais tarde');
+    catch (error) {
+      console.log(error)
+      toast.error('Erro ao cadastrar fornecedor, tente novamenta mais tarde!!');
     }
   }
 
   return (
     <>
-      {/* <AdvancedSideBar /> */}
-      <div className="provider_container">
+      <div className="provider-container">
         <ToastContainer
           position="top-center"
           autoClose={5000}
@@ -62,8 +66,8 @@ export default function Provider() {
           draggable
           pauseOnHover
         />
-        <div className="provider_container__title">
-          <h1 className="provider_container__title">Cadastro de fornecedores</h1>
+        <div className="provider-container__title">
+          <h1 className="provider-container__title">Cadastro de fornecedores</h1>
         </div>
 
         <Divider style={{ borderWidth: 2, borderColor: "white" }} />
@@ -75,8 +79,8 @@ export default function Provider() {
                 <AdvancedInput
                   label="Razão social"
                   placeholder="Nome"
-                  value={social}
-                  onChange={e => setSocial(e.target.value)}
+                  value={company_name}
+                  onChange={e => setCompany_name(e.target.value)}
                 />
               </Col>
             </Row>
@@ -84,89 +88,93 @@ export default function Provider() {
 
           <div className="provider-form__input">
             <Row gutter={16}>
-              <Col sm={12} xs={12} md={12} lg={11}>
-                <AdvancedInput
-                  label="CPF"
-                  mask="999.999.999-99"
-                  value={documents}
-                  onChange={e => setDocuments(e.target.value)}
-                />
-              </Col>
-              <Col sm={12} xs={12} md={12} lg={11}>
-                <AdvancedInput
-                  label="CNPJ"
-                  mask="99.999.999/9999-99"
-                  value={documents}
-                  onChange={e => setDocuments(e.target.value)}
-                />
-              </Col>
-              <Col sm={12} xs={12} md={12} lg={11}>
-                <AdvancedInput
-                  label="I.E"
-                  placeholder="508.127.628-60"
-                  value={ie}
-                  onChange={e => setIe(e.target.value)}
-                />
-              </Col>
+              <div style={{ display: "flex", justifyContent: 'space-around' }}>
+                <Col span={8}>
+                  <AdvancedInput
+                    label="CPF"
+                    mask="999.999.999-99"
+                    value={cpf}
+                    onChange={e => setCpf(e.target.value)}
+                  />
+                </Col>
+                <Col span={8} >
+                  <AdvancedInput
+                    label="CNPJ"
+                    mask="99.999.999/9999-99"
+                    value={cnpj}
+                    onChange={e => setCnpj(e.target.value)}
+                  />
+                </Col>
+                <Col span={8} >
+                  <AdvancedInput
+                    label="I.E"
+                    placeholder="508.127.628-60"
+                    value={state_registration}
+                    onChange={e => setState_registration(e.target.value)}
+                  />
+                </Col>
+              </div>
             </Row>
           </div>
 
           <div className="provider-form__input">
             <Row gutter={16}>
-
-              <Col sm={12} xs={12} md={12} lg={6}>
-                <AdvancedInput
-                  label="CEP"
-                  mask="99999-999"
-                  placeholder="13070-172"
-                  value={cep}
-                  onChange={e => setCep(e.target.value)}
-                />
-              </Col>
-
-              <Col sm={12} xs={12} md={12} lg={16}>
-                <AdvancedInput
-                  label="Endereço"
-                  placeholder="Av Marechal Rondon"
-                  value={address}
-                  onChange={e => setAddress(e.target.value)}
-                />
-              </Col>
+              <div style={{ display: "flex", justifyContent: 'space-around' }}>
+                <Col span={12} >
+                  <AdvancedInput
+                    label="CEP"
+                    mask="99999-999"
+                    placeholder="13070-172"
+                    value={cep}
+                    onChange={e => setCep(e.target.value)}
+                  />
+                </Col>
+                <Col span={12}>
+                  <AdvancedInput
+                    label="Endereço"
+                    placeholder="Av Marechal Rondon"
+                    value={address}
+                    onChange={e => setAddress(e.target.value)}
+                  />
+                </Col>
+              </div>
             </Row>
           </div>
 
           <div className="provider-form__input">
             <Row gutter={16}>
-              <Col sm={8} xs={8} md={6} lg={4}>
-                <AdvancedInput
-                  label="Número"
-                  placeholder="2044"
-                  value={number}
-                  onChange={e => setNumber(e.target.value)}
-                />
-              </Col>
-              <Col sm={8} xs={8} md={12} lg={14}>
-                <AdvancedInput
-                  label="Bairro"
-                  placeholder="Jardin Chapadão"
-                  value={district}
-                  onChange={e => setDistrict(e.target.value)}
-                />
-              </Col>
-              <Col sm={8} xs={8} md={6} lg={4}>
-                <AdvancedInput
-                  label="Complemento"
-                  placeholder="apto 33"
-                  value={complements}
-                  onChange={e => setComplements(e.target.value)}
-                />
-              </Col>
+              <div style={{ display: "flex", justifyContent: 'space-around' }}>
+                <Col span={8}>
+                  <AdvancedInput
+                    label="Número"
+                    placeholder="2044"
+                    value={number}
+                    onChange={e => setNumber(e.target.value)}
+                  />
+                </Col>
+                <Col span={8}>
+                  <AdvancedInput
+                    label="Bairro"
+                    placeholder="Jardin Chapadão"
+                    value={neighborhood}
+                    onChange={e => setNeighborhood(e.target.value)}
+                  />
+                </Col>
+                <Col span={8}>
+                  <AdvancedInput
+                    label="Complemento"
+                    placeholder="apto 33"
+                    value={complement}
+                    onChange={e => setComplement(e.target.value)}
+                  />
+                </Col>
+              </div>
             </Row>
           </div>
 
           <div className="provider-form__input">
             <Row gutter={16}>
-              <Col sm={18} xs={18} md={15} lg={14}>
+              <Col span={12}>
                 <AdvancedInput
                   label="Munícipio"
                   placeholder="Campinas"
@@ -174,11 +182,11 @@ export default function Provider() {
                   onChange={e => setCity(e.target.value)}
                 />
               </Col>
-              <Col sm={6} xs={6} md={9} lg={8}>
+              <Col span={12}>
                 <AdvancedInput
                   label="UF"
-                  value={uf}
-                  onChange={e => setUf(e.target.value)}
+                  value={state}
+                  onChange={e => setState(e.target.value)}
                 />
               </Col>
             </Row>
@@ -186,15 +194,16 @@ export default function Provider() {
 
           <div className="provider-form__input">
             <Row gutter={16}>
-              <Col sm={12} xs={12} md={12} lg={11}>
+              <Col span={12}>
                 <AdvancedInput
                   label="Telefone"
+                  mask="(99) 99999-9999"
                   placeholder="(19) 99755-2541"
                   value={phone}
                   onChange={e => setPhone(e.target.value)}
                 />
               </Col>
-              <Col sm={12} xs={12} md={12} lg={11}>
+              <Col span={12}>
                 <AdvancedInput
                   label="E-mail"
                   placeholder="lucas.araujo@mblabs.com"
@@ -206,24 +215,18 @@ export default function Provider() {
           </div>
 
           <div className="provider-form__button">
-            <Row gutter={16} justify="space-around">
-              <Col sm={4} xs={8} md={8} lg={4}>
-                <Button type="submit" className="provider-form__button__delete">
-                  Excluir
-              </Button>
-              </Col>
-              <Col sm={4} xs={8} md={8} lg={4}>
-                <Button
-                  type="submit"
-                  className="provider-form__button__search"
-                >
-                  Procurar
-                </Button>
-              </Col>
-              <Col sm={4} xs={8} md={8} lg={4}>
-                <Button type="submit" className="provider-form__button__include">Incluir</Button>
-              </Col>
-            </Row>
+            <Button type="submit" className="provider-form__button__search">
+              Procurar
+            </Button>
+            <button type="submit" className="provider-form__button__include">
+              Incluir
+            </button>
+            <Button type="submit" className="provider-form__button__delete">
+              Excluir
+            </Button>
+            <Button type="submit" className="provider-form__button__save">
+              Salvar
+            </Button>
           </div>
         </form>
       </div>
